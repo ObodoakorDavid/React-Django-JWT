1adf8bd806bfe230256e1f6561d9c0e46bdc59aa
ObodoakorDavid <obodoakordavid@outlook.com>
2022-10-22 19:39:24 +0100

    first commit

---
+++ server/api/views.py
@@ -0,0 +1,50 @@
+import json
+from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+# Create your views here.
+# def getRoutes(request):
+#     # routes = [
+#     #     '/api/token',
+#     #     '/api/token/refresh/'
+#     # ]
+
+#     routes = {
+#         'token': '/api/token',
+#         'token_refresh': '/api/token/refresh'
+#     }
+#     return JsonResponse(routes)
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['name'] = user.name
+        # ...
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        'api/token',
+        '/api/token/refresh/'
+    ]
+
+    # routes = {
+    #     'token': '/api/token',
+    #     'token_refresh': '/api/token/refresh'
+    # }
+    return Response(routes)
+
+
